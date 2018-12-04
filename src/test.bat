26db3fbf5f7f9838735b7bee561ba89f11742745
yallie <yallie@yandex.ru>
2018-12-04 13:18:51 +0300

    Enabled .NET Core 2.0 target in NetMQ unit tests.

--- src/test.bat
+++ src/test.bat
@@ -2,5 +2,6 @@
 
 dotnet test JsonServices.Core.Tests
 dotnet test JsonServices.Serialization.Tests
-dotnet test JsonServices.Transport.WebSocketSharp.Tests
 dotnet test JsonServices.Transport.Fleck.Tests
+dotnet test JsonServices.Transport.NetMQ.Tests
+dotnet test JsonServices.Transport.WebSocketSharp.Tests
