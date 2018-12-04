8ae6b7bc8c2ae791ee93e1cf75bc46fa1e9f7955
yallie <yallie@yandex.ru>
2018-12-04 13:39:09 +0300

    Minor cleanup.

--- src/JsonServices.Core/Events/ServerSubscriptionManager.cs
+++ src/JsonServices.Core/Events/ServerSubscriptionManager.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using JsonServices.Messages;
 using JsonServices.Transport;
 
 namespace JsonServices.Events
--- src/JsonServices.Core/Events/SubscriptionService.cs
+++ src/JsonServices.Core/Events/SubscriptionService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using JsonServices.Exceptions;
-using JsonServices.Services;
+﻿using JsonServices.Services;
 
 namespace JsonServices.Events
 {
