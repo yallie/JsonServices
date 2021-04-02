0e4a7c132077ecac0707bfee93f41734efc6be52
yallie <yallie@yandex.ru>
2021-04-03 00:36:20 +0300

    Added Properties to JsonIdentity.

---
+++ src/JsonServices.Core.Tests/Auth/JsonIdentityTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using JsonServices.Auth;
+using NUnit.Framework;
+
+namespace JsonServices.Tests.Auth
+{
+	[TestFixture]
+	public class JsonIdentityTests
+	{
+		private class AnonIdentity : IIdentity
+		{
+			public string Name => "Anonymous";
+
+			public string AuthenticationType => "None";
+
+			public bool IsAuthenticated => false;
+		}
+
+		[Test]
+		public void JsonIdentityBasedOnIIdentity()
+		{
+			var anon = new AnonIdentity();
+			var identity = new JsonIdentity(anon);
+			Assert.AreEqual(anon.Name, identity.Name);
+			Assert.AreEqual(anon.AuthenticationType, identity.AuthenticationType);
+		}
+
+		[Test]
+		public void JsonIdentityBasedOnJsonIdentity()
+		{
+			var identity = new JsonIdentity("Hello", "World");
+			identity.Properties["Date"] = DateTime.Now.ToString();
+
+			var copy = new JsonIdentity(identity);
+			Assert.AreEqual(identity, copy);
+		}
+
+		[Test]
+		public void JsonIdentityEqualityTests()
+		{
+			Assert.That(new JsonIdentity(), Is.Not.EqualTo(null));
+			Assert.That(new JsonIdentity(), Is.EqualTo(new JsonIdentity()));
+			Assert.That(new JsonIdentity("Hello", "World"), Is.EqualTo(new JsonIdentity("Hello", "World")));
+			Assert.That(new JsonIdentity("Hello", "World"), Is.Not.EqualTo(new JsonIdentity("Goodbye", "World")));
+
+			var a = new JsonIdentity("A", "B");
+			a.Properties["C"] = "D";
+			var b = new JsonIdentity("A", "B");
+			b.Properties["C"] = "D";
+			Assert.AreEqual(a, b);
+
+			b.Properties["D"] = "E";
+			Assert.AreNotEqual(a, b);
+		}
+	}
+}
--- src/JsonServices.Core/Auth/JsonIdentity.cs
+++ src/JsonServices.Core/Auth/JsonIdentity.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace JsonServices.Auth
 {
-	public class JsonIdentity : IIdentity
+	public class JsonIdentity : IIdentity, IEquatable<JsonIdentity>
 	{
 		public JsonIdentity()
 		{
@@ -19,15 +19,63 @@ namespace JsonServices.Auth
 			AuthenticationType = type;
 		}
 
-		public JsonIdentity(IIdentity identity)
-			: this(identity?.Name, identity?.AuthenticationType)
+		public JsonIdentity(IIdentity identity) =>
+			CopyFrom(identity);
+
+		private void CopyFrom(IIdentity identity)
 		{
+			Name = identity.Name;
+			AuthenticationType = identity.AuthenticationType;
+			if (identity is JsonIdentity ji)
+			{
+				Properties = ji.Properties.ToDictionary(p => p.Key, p => p.Value);
+			}
 		}
+
+		public bool Equals(JsonIdentity other)
+		{
+			if (other == null)
+			{
+				return false;
+			}
+
+			if (Name != other.Name || AuthenticationType != other.AuthenticationType)
+			{
+				return false;
+			}
+
+			if (Properties.Count != other.Properties.Count)
+			{
+				return false;
+			}
+
+			foreach (var p in Properties)
+			{
+				if (!other.Properties.TryGetValue(p.Key, out var value))
+				{
+					return false;
+				}
+
+				if (value != p.Value)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		public override bool Equals(object obj) => Equals(obj as JsonIdentity);
+
+		public override int GetHashCode() => Name.GetHashCode();
 
 		public string Name { get; set; }
 
 		public string AuthenticationType { get; set; }
 
 		public virtual bool IsAuthenticated => !string.IsNullOrEmpty(AuthenticationType);
+
+		public Dictionary<string, string> Properties { get; private set; } =
+			new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 	}
 }
